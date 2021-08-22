82d9d3fbb8365d59405d99a0df0d1a19c00e9e00
khushbuv <34154202+khushbuv@users.noreply.github.com>
2021-08-22 12:02:05 +0200

    added changes

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            welpen_image: './assets/images/dogs-nav.png',
+            sitter1_image: './assets/images/sitter1.jpeg',
+            sitter2_image: './assets/images/sitter2.jpeg',
+            sitter3_image: './assets/images/sitter3.jpeg',
+            sitter4_image: './assets/images/sitter4.jpeg',
+            details: ['50% cotton', '30% wool', '20% polyester']
+        }
+    }
+})
